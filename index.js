4fac1b03794dcd5db8260b5b1919b33fd668ca0d
Jade Huynh <35jadeh@gmail.com>
2019-07-06 17:04:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,8 +53,12 @@ function removeFromCart(item) {
   
   for(var i = 0; i < cart.length; i++)
   {
-    if(item === cart[i].itemName){cart.splice(i, 1, )}
-    else{"That item is not in your cart."}
+    if(item === cart[i].itemName)
+    {
+      cart.splice(i, 1)
+      
+    }
+    else{return "That item is not in your cart."}
   }
 }
 
